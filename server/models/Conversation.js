ac0e6e02e13e37a1f3ec656997497768672d30b4
eric <erictranchell@yahoo.com>
2023-07-12 19:58:40 -0400

    conversation model

---
+++ server/models/Conversation.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const conversationSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    partcipants: [{
+        type: String,
+        ref: 'User',
+    }],
+    messages: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Message',
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+const Conversation = model('Conversation', conversationSchema);
+
+module.exports = Conversation;
