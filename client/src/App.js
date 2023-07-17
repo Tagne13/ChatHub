e6e61239d94489a2370325471ede6e52ff7c6535
El Jefe <Tagne13@gmail.com>
2023-07-17 19:11:28 -0400

    attempting to fix merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,14 @@ import Register from './components/Register';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-
-const client = new ApolloClient ({
-  uri: '/graphql' ,
+const client = new ApolloClient({
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
-    <ApolloProvider client ={ client}>
+    <ApolloProvider client={client}>
       <Router>
         <div>
           <Routes>
@@ -23,7 +22,7 @@ function App() {
         </div>
       </Router>
     </ApolloProvider>
-  )
+  );
 }
 
 export default App;
---
+++ client/src/components/home/Signup.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Signup = () => () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlfor="username">username</label>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          placeholder="username"
+          id="username"
+          name="username"
+        />
+        <label htmlfor="email">email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+        />
+        <label htmlfor="password">password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+        <button type="submit">Login</button>
+      </form>
+
+      <button onClick={() => props.onFormSwitch("login")}>
+        If you already have an account. Login here!
+      </button>
+    </>
+  );
+};
+
+export default Signup;
