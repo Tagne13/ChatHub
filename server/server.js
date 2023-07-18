4655a947fd20cc82e05627febe7839669872ce02
El Jefe <Tagne13@gmail.com>
2023-07-17 20:04:01 -0400

    set up socket.io server-side for real-time messages

--- server/server.js
+++ server/server.js
@@ -40,6 +40,45 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
 };
+
+const io = require('socket.io')(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: 'http://localhost:3001',
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Connected to socket.io!');
+  socket.on('setup', (userData) => {
+    socket.join(userData._id);
+    socket.emit('connected');
+  });
+
+  socket.on('join chat', (room) => {
+    socket.join(room);
+    console.log('User Joined Room: ' + room);
+  });
+  socket.on('typing', (room) => socket.in(room).emit('typing'));
+  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
+
+  socket.on('new message', (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
+
+    if (!chat.users) return console.log('chat.users not defined');
+
+    chat.users.forEach((user) => {
+      if (user.id == newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit('message received', newMessageReceived);
+    });
+  });
+
+  socket.off('setup', () => {
+    console.log('USER DISCONNECTED');
+    socket.leave(userData._id);
+  });
+});
   
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
