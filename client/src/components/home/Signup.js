fd54d3f5768be8c2029663262cfa168b23cd7e25
KnightHawk1090 <121210332+KnightHawk1090@users.noreply.github.com>
2023-07-20 21:11:06 -0500

    sign up page

--- client/src/components/home/Signup.js
+++ client/src/components/home/Signup.js
@@ -60,45 +60,62 @@ const Signup = (props) => {
   };
 
   return (
-    <>
-      <form onSubmit={handleFormSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          value={userFormData.username}
-          onChange={handleInputChange}
-          type="text"
-          placeholder=" Your username"
-          id="username"
-          name="username"
-          required
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          value={userFormData.email}
-          onChange={handleInputChange}
-          type="email"
-          placeholder="Your email address"
-          id="email"
-          name="email"
-          required
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          value={userFormData.password}
-          onChange={handleInputChange}
-          type="password"
-          placeholder="********"
-          id="password"
-          name="password"
-          required
-        />
-        <button type="submit">Sign Up!</button>
+    <div  className="flex flex-col items-center justify-center h-screen bg-cover bg-center" style={{backgroundImage: 'url(https://img.freepik.com/free-vector/speech-bubble-vector-halftone-style_53876-126726.jpg?w=826&t=st=1689814164~exp=1689814764~hmac=fa98d63ebf059e3d8b8f048045122f50a8ffd7be3285960d3e0f3ea3aa52c752)' }}>
+      <form onSubmit={handleFormSubmit} className="max-w-xs">
+        <div>
+          <label htmlFor="username" className="mb-1 text-3xl text-violet-400">Username</label>
+          <div className="flex">
+            <input
+              value={userFormData.username}
+              onChange={handleInputChange}
+              type="text"
+              placeholder=" Your username"
+              id="username"
+              name="username"
+              required
+              className="border border-gray-300 rounded py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+        </div>
+
+        <div className="mb-2">
+        <label htmlFor="email" className="mb-1 text-3xl text-violet-400">Email</label>
+          <div className="flex">
+            <input
+              value={userFormData.email}
+              onChange={handleInputChange}
+              type="email"
+              placeholder="Your email address"
+              id="email"
+              name="email"
+              required
+              className="border border-gray-300 rounded py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+        </div>
+
+        <div className="mb-2">
+        <label htmlFor="password" className="mb-1 text-3xl text-violet-400">Password</label>
+          <div className="flex">
+          <input
+            value={userFormData.password}
+            onChange={handleInputChange}
+            type="password"
+            placeholder="********"
+            id="password"
+            name="password"
+            required
+            className="border border-gray-300 rounded py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+          </div>
+        </div>
+        <button type="submit" className="bg-cyan-400 hover:bg-yellow-300 text-green-800 font-bold py-2 px-4 mt-1.5 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Sign Up!</button>
       </form>
 
-      <button onClick={() => props.onFormSwitch("login")}>
+      <button onClick={() => props.onFormSwitch("login")} className="text-stone-950 hover:text-yellow-300 mt-4">
         If you already have an account. Login here!
       </button>
-    </>
+    </div>
   );
 };
 
