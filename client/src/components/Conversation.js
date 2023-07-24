8bb22e539eca3e18fd6b69f0e963cb9527f994d7
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-07-24 16:03:03 -0400

    Corrected the get conversations query and wrote in the props to pass down.

--- client/src/components/Conversation.js
+++ client/src/components/Conversation.js
@@ -4,7 +4,6 @@ import MessageInput from "./Conversation/MessageInput";
 // import Navbar from "./Conversation/Navbar";
 import { GET_CONVERSATIONS } from "../utils/queries";
 import { useQuery } from "@apollo/client";
-import jwt_decode from "jwt-decode";
 import Auth from "../utils/auth";
 
 // Query the conversation.
@@ -13,24 +12,24 @@ function Conversation() {
   if (Auth.loggedIn()) {
   }
   const user = Auth.getProfile();
-  console.log(user.data._id);
 
-  const { loading, data, error } = useQuery(GET_CONVERSATIONS, {
-    variables: { userId: user.data._id },
-  });
+  const { loading, data, error } = useQuery(GET_CONVERSATIONS);
 
   if (loading) {
     return <p>Loading conversation...</p>;
   }
   if (error) {
-    return <p>Error fetching messages: {error.name}</p>;
+    return <p>Error fetching: {error.message}</p>;
   }
 
   return (
     <>
       {/* <Navbar /> */}
-      <Messages />
-      <MessageInput />
+      <Messages conversationId={data.getConversations[0]._id} />
+      <MessageInput
+        conversationId={data.getConversations[0]._id}
+        senderId={user.data._id}
+      />
     </>
   );
 }
